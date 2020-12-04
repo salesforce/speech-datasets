ef5601034250c060add1eb1f5ebb078444bd43ee
young mo kang <ykang@salesforce.com>
2020-12-04 10:52:52 +0000

    update url

--- tools/install_sph2pipe.sh
+++ tools/install_sph2pipe.sh
@@ -9,7 +9,7 @@ pwd=$PWD
 dir=$1
 
 if [ ! -e sph2pipe_v2.5.tar.gz ]; then
-  wget --no-check-certificate https://www.ldc.upenn.edu/sites/www.ldc.upenn.edu/files/ctools/sph2pipe_v2.5.tar.gz
+  wget --no-check-certificate https://www.openslr.org/resources/3/sph2pipe_v2.5.tar.gz
 fi
 
 tar xzvf sph2pipe_v2.5.tar.gz -C $dir
